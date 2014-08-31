8c14bf12d9bff93dde2a4b483972f30e0586c4c8
RokitSalad <peterdobbs@hotmail.com>
2014-08-31 16:20:24 +0100

    Just started to work out how to use the client. The ClosedEvent class contains the detail which is needed to get things going. Need to work out the architecture.

---
+++ Helpful.CircuitBreaker.Events.EventStore/ClosedEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Common.Utils;
+using EventStore.ClientAPI.SystemData;
+
+namespace Helpful.CircuitBreaker.Events.EventStore
+{
+    public class ClosedEvent : IClosedEvent
+    {
+        public void RaiseEvent(object breakerConfig)
+        {
+            var connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1113));
+            connection.Connect();
+            UserCredentials credentials = new UserCredentials("dFine", "Password02");
+            connection.AppendToStream("use the breaker name", ExpectedVersion.Any, credentials, breakerConfig.AsJson());
+        }
+    }
+}
